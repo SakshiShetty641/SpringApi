7e3677f1b7d2c899bcf55c2c8c6dfcc26b9fc8ac
sakshishetty1231997@gmail.com <SakshiShetty641>
2024-03-22 12:38:32 +0530

    Spring Api With Controller class

---
+++ src/main/java/com/example/LearningSpringApis/LearningSpringApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningSpringApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApisApplication.class, args);
+	}
+
+}
